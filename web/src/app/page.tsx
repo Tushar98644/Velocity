8631bab4fa0238f622bffcdebaf99840c4f0f87a
Tushar Banik <b.toshar@iitg.ac.in>
2025-08-04 18:05:09 +0530

    ui

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,10 @@
-'use client'
+"use client"
 
-import { authClient } from "@/lib/auth.client";
-import { useRouter } from "next/navigation";
+import { redirect } from 'next/navigation'
 
-const HomePage = () => {
 
-  const router = useRouter();
-
-  return (
-    <main>
-      <h1>Welcome to the Velocity Dashboard</h1>
-      <button onClick={async () => await authClient.signIn.social({
-        provider: "google",
-        callbackURL: "http://localhost:3000/dashboard",
-      }
-      )}>
-        Sign In with Google
-      </button>
-    </main>
-  );
+function Home() {
+  redirect('app/dashboard')
 }
 
-export default HomePage;
+export default Home
---
+++ web/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7B6CF0'
+      },
+      fontSize: {
+        xxs: '0.625rem'
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
